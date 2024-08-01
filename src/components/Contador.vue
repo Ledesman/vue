c9f444736ca6171f7f06ab5ef8ffab7240aaebc1
ledesman <ledesmanicolas079@gmail.com>
2024-07-31 22:33:58 -0300

    Se realizo el segundo cambio

---
+++ src/components/Contador.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+  <h2>Hola mundo</h2>
+  <p>{{ contador }} </p>
+  <button @click="Incrementar">Incrementar</button>
+  <button @click="Decrementar">Decrementar</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const contador = ref(0);
+  const Incrementar = () =>{
+    contador.value++;
+  }
+  const Decrementar = () =>{
+    contador.value--;
+  }
+  
+  
+  </script>
+  
+  <style  scoped>
+  p{
+    color: red;
+  }
+  </style>
