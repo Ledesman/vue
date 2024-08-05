d754ed152359455bad183d68c9b736ed63865f56
ledesman <ledesmanicolas079@gmail.com>
2024-08-05 09:29:41 -0300

    Creamos el main y ruteas

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+
+
+<template>
+    <div class="sidebar">
+        <h2>Barra Lateral</h2>
+        <RouterLink to="/contador">Contador</RouterLink>
+        <RouterLink to="/lista-de-tareas">Lista de Tareas</RouterLink>
+    </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+
+
+</script>
+
+<style scoped>
+
+.sidebar {
+    width: 200px;
+    background-color: #f0f0f0;
+}
+</style>
---
+++ src/views/main.vue
@@ -0,0 +1,25 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+// eslint-disable-next-line vue/multi-word-component-names
+<template>
+    <div class="container">
+        <Sidebar></Sidebar>
+        <div class="content">
+            <RouterView></RouterView>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+import Sidebar from '../components/Sidebar.vue';
+</script>
+
+<style scoped>
+.container{
+    display: flex;
+}
+.content{
+    flex: 1 ;
+    padding: 20px;
+}
+</style>
